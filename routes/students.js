03a9e29b9900c503f2b6e38ac3d8ce948073fa92
riyaverma85 <riyaverma2976@gmail.com>
2025-07-19 21:46:15 -0700

    about

--- routes/students.js
+++ routes/students.js
@@ -5,5 +5,8 @@ const route=express.Router();
 route.get("./home",(req,res)=>{
     res.send("Welcome to the student home page");
 })
+route.get("./about",(req,res)=>{
+    res.send("Welcome to the student home page");
+})
 
 module.exports=route;
